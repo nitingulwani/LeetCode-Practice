e09f0e716202382970fea37dd3adefdbb45e80d4
Nitin <gulwanin35@gmail.com>
2023-05-03 00:34:30 +0530

    added files

---
+++ signOfProductofanArray.java
@@ -0,0 +1,15 @@
+class signOfProductofanArray {
+    public int arraySign(int[] nums) {
+        double product = 1;
+        for(int i = 0; i<nums.length; i++){
+            product *= nums[i];
+        }
+        if(product > 0){
+            return 1;
+        }else if(product < 0){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+}
