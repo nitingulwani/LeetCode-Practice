8bc3abffd8fbefd4fd251848fa061b12212c41b0
Nitin <gulwanin35@gmail.com>
2023-05-11 01:40:21 +0530

    added files

---
+++ searchInsert.java
@@ -0,0 +1,21 @@
+class searchInsert {
+    public int searchInsert(int[] nums, int target) {
+        int si=0;
+        int ei=nums.length-1;
+        while(si<=ei){
+            
+            int mid=si+(ei-si)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                ei=mid-1;
+            }
+            else{
+                si=mid+1;
+            }
+        }
+        return si;   
+    }
+}
