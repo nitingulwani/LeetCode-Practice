b27d62f73b0e346afe9e537c9479f95b39bcd75e
Nitin <gulwanin35@gmail.com>
2023-05-17 01:12:31 +0530

    added maxDepthbt

---
+++ maxDepthBt.java
@@ -0,0 +1,21 @@
+class maxDepthBt {
+    public int maxDepth(TreeNode root) {
+
+        if(root == null){
+            return 0;
+        }
+
+        else{
+            int lDepth = maxDepth(root.left);
+            int rDepth = maxDepth(root.right);
+
+            if(lDepth>rDepth){
+                return (lDepth+1);
+        }
+            else{
+                return (rDepth+1);
+            }
+        }
+       
+    }
+}
