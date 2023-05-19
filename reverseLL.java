f1b6579ea77a9d9cdfdfdf4394afc0095cc55546
Nitin <gulwanin35@gmail.com>
2023-05-20 02:55:57 +0530

    changed reverseLL

--- reverseLL.java
+++ reverseLL.java
@@ -15,3 +15,16 @@ class reverseLL {
     }
 }
 
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         // base case
+//         if(head == null || head.next == null) return head;
+        
+//         ListNode newHead = reverseList(head.next);
+        
+//         head.next.next = head;
+//         head.next = null;
+
+//         return newHead;
+//     }
+// }
