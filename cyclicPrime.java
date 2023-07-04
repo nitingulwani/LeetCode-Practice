08f729ce8dd00741d44eabac0c6be86f55d17f0c
Nitin <gulwanin35@gmail.com>
2023-07-04 10:00:44 +0530

    added leaders

---
+++ cyclicPrime.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class cyclicPrime {
+    public static boolean isPrime(int n){
+        for(int i = 2; i<n; i++){
+            
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+
+        
+    }
+}
---
+++ leadersOfAnArray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class leadersOfAnArray {
+    static ArrayList<Integer> leaders(int arr[], int n){
+       
+        ArrayList<Integer> ans = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+        st.push(arr[arr.length-1]);
+        ans.add(st.peek());
+        for(int i = arr.length-2; i>=0; i--){
+            if(st.peek() <= arr[i]){
+                st.pop();
+                st.push(arr[i]);
+                ans.add(arr[i]);
+            }
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        leaders(arr, n);
+
+
+    }
+}
